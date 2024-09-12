1c7be9e97c84224579c2ac373ed39cd78eb7d4e4
Ayush <ayushnigam843@gmail.com>
2024-09-12 12:46:40 +0530

    Added React Redux

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import NoteState from './context/notes/noteState';
 import About from './components/About';
 import { useState } from 'react';
-
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+const count = useSelector((state) => state.counter.value)
+const dispatch = useDispatch()
 function App() {
   const [count, setCount] = useState(0)
   const [count2, setCount2] = useState(1)
+  const reduxCount = useSelector((state) => state.counter.value)
 
   return (
     <>
@@ -16,6 +20,19 @@ function App() {
           <button onClick={() => setCount(count + 1)}>{count}</button>
           <button onClick={() => setCount2(count2 + 1)}>{count2}</button>
         </div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
       </NoteState>
     </>
   );
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -2,12 +2,12 @@ import React, { useMemo } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const state = useMemo(() => ({ name:"Harry", class:"5th"}),[]) 
-return (
-    <NoteContext.Provider value={state}>
-        {props.children}
-    </NoteContext.Provider>
-)
+    const state = useMemo(() => ({ name: "Harry", class: "5th" }), [])
+    return (
+        <NoteContext.Provider value={state}>
+            {props.children}
+        </NoteContext.Provider>
+    )
 }
 
 export default NoteState;
